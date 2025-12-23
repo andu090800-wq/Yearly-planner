345d80d3fb17bcae639c0068bf47f1daa181f42d
andu090800-wq <andu090800@yahoo.com>
2025-12-23 12:05:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "plans-glass-v9";
+const CACHE = "plans-glass-v10";
 const ASSETS = [".", "index.html", "styles.css", "app.js", "manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
